b2548e95ad5d50ba7b9656119a37627bff96a821
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-03-04 23:14:49 +0300

    Initial Commit

---
+++ TestBranches/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestBranches
+//
+//  Created by Batuhan Baran on 4.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
